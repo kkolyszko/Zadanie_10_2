216f9cd8be3c59378b88bb13b7bfd3daff1180e5
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-17 20:02:39 +0200

    trochę kodu do przycisków

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ $(function () {
         carouselList.css({marginLeft:0});
     };
     
+    function moveLastSlide () {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        
+        lastItem.before(firstItem);
+        carouselList.css({marginLeft:0});
+    };
     
+    $("#right").click(function() {
+    changeSlide ();
+});
+    
+    $("#left").click(function() {
+        moveLastSlide();
+        carouselList.animate({'marginLeft':+400}, 500);
+});
     
 });
