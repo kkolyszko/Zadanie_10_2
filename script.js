80f8c69232d3af32d5917b032f9ffbe9fe3f9dda
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-17 21:55:49 +0200

    dodane skrypty do przycisków

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function () {
     var carouselList = $("#carousel ul");
-    setInterval(changeSlide, 3000);
+    var myInterval = setInterval(changeSlide, 3000);
     
     function changeSlide () {
         carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
@@ -17,18 +17,24 @@ $(function () {
     function moveLastSlide () {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
-        
+        carouselList.css({marginLeft:-400});
         lastItem.before(firstItem);
-        carouselList.css({marginLeft:0});
     };
     
+    function refreashInterval () {
+        clearInterval(myInterval);
+        setTimeout(function(){setInterval(changeSlide, 3000)},5000);
+    };
+
     $("#right").click(function() {
-    changeSlide ();
-});
+        changeSlide();
+        refreashInterval();
+    });
     
     $("#left").click(function() {
         moveLastSlide();
-        carouselList.animate({'marginLeft':+400}, 500);
-});
+        carouselList.animate({'marginLeft':0}, 500);
+        refreashInterval();
+    });
     
 });
