b8291b73c5438cf1d13f97bfa658f66d304eaeec
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-21 14:45:19 +0200

    myInterval jako funkcja i clear na hooverze

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 $(function () {
     var carouselList = $("#carousel ul");
-    var myInterval = setInterval(changeSlide, 3000);
     
-    function changeSlide () {
-        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    
+    function startInterval () {
+        var myInterval = setInterval(changeSlideRight, 3000);
+    };
+    
+    startInterval();
+    
+    function changeSlideRight () {
+        carouselList.animate({'marginLeft':-800}, 500, moveFirstSlide);
+    };
+    
+    function changeSlideLeft () {
+        carouselList.animate({'marginLeft':0}, 500, moveLastSlide);
     };
     
     function moveFirstSlide () {
@@ -11,29 +21,35 @@ $(function () {
         var lastItem = carouselList.find("li:last");
         
         lastItem.after(firstItem);
-        carouselList.css({marginLeft:0});
+        carouselList.css({marginLeft:-400});
     };
     
     function moveLastSlide () {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
+        
         carouselList.css({marginLeft:-400});
-        lastItem.before(firstItem);
+        firstItem.before(lastItem);
     };
     
     function refreashInterval () {
         clearInterval(myInterval);
-        setTimeout(function(){setInterval(changeSlide, 3000)},5000);
+        startInterval();
     };
+    
+    $("#right").mouseover(function() {
+        clearInterval(myInterval);
+        setTimeout(startInterval(), 5000);
+        console.log("Hi");
+    });
 
     $("#right").click(function() {
-        changeSlide();
+        changeSlideRight();
         refreashInterval();
     });
     
     $("#left").click(function() {
-        moveLastSlide();
-        carouselList.animate({'marginLeft':0}, 500);
+        changeSlideLeft();
         refreashInterval();
     });
     
